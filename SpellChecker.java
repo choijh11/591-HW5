d223fef90972126f758c21fed361eabe9fb755a0
alecrgraham <89941163+alecrgraham@users.noreply.github.com>
2021-11-11 13:03:06 -0500

    Add files via upload
    Merging Alec's SpellChecker with Jae's.

--- SpellChecker.java
+++ SpellChecker.java
@@ -1,50 +1,52 @@
-import java.util.Scanner;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner; 
 
 public class SpellChecker {
-
-	public SpellChecker() {
-		// TODO: You can modify the body of this constructor,
-		// or you can leave it blank. You must keep the signature, however.
-	}
-
-	public void start() {
-
-		Scanner scnr = new Scanner(System.in);
-
-		// prompt user for a dictionary to use for spell check
-		System.out.printf(Util.DICTIONARY_PROMPT);
-		String dictionaryChoice = scnr.nextLine();
-
-		try {
-			FileInputStream dictionaryStream = new FileInputStream(dictionaryChoice);
-		} catch (FileNotFoundException e) {
-			System.out.printf(Util.FILE_OPENING_ERROR);
-			System.out.printf(Util.DICTIONARY_PROMPT);
-			dictionaryChoice = scnr.nextLine();
-		}
-
-		System.out.printf(Util.DICTIONARY_SUCCESS_NOTIFICATION, dictionaryChoice);
-
-		// prompt user for a file to spell check
-		System.out.printf(Util.FILENAME_PROMPT);
-		String fileChoice = scnr.nextLine();
-		
-		try {
-			FileOutputStream spellChecked = new FileOutputStream(fileChoice);
-		} catch (FileNotFoundException e) {
-			System.out.printf(Util.FILE_OPENING_ERROR);
-			System.out.printf(Util.FILENAME_PROMPT); 
-			fileChoice = scnr.nextLine(); 
-		}
-		
-		System.out.printf(Util.FILE_SUCCESS_NOTIFICATION, fileChoice); 
-	}
-
-	public static void main(String[] args) {
-		SpellChecker sc = new SpellChecker(); 
-		sc.start();
-	}
-}
+    public SpellChecker() {
+      // TODO: You can modify the body of this constructor,
+      // or you can leave it blank. You must keep the signature, however.
+    }
+    
+    public static boolean canOpen(String fileName) {
+    	try {
+    		FileInputStream is = new FileInputStream(fileName);
+    	} catch (IOException e) {
+    		return false;
+    	}
+    	return true;
+    }
+  
+    public static void start() throws IOException {
+      // TODO: You can modify the body of this method,
+      // or you can leave it blank. You must keep the signature, however.
+    	Scanner s = new Scanner(System.in);
+    	System.out.printf(Util.DICTIONARY_PROMPT);
+    	String dictionaryName = s.nextLine();
+    	while (canOpen(dictionaryName) == false) {
+    		System.out.printf(Util.FILE_OPENING_ERROR);
+        	System.out.printf(Util.DICTIONARY_PROMPT);
+        	dictionaryName = s.nextLine();
+    	}
+    	System.out.printf(Util.DICTIONARY_SUCCESS_NOTIFICATION, dictionaryName);
+    	System.out.printf(Util.FILENAME_PROMPT);
+    	String fileName = s.nextLine();
+    	while (canOpen(fileName) == false) {
+    		System.out.printf(Util.FILE_OPENING_ERROR);
+        	System.out.printf(Util.FILENAME_PROMPT);
+    	}
+    	String outputFileName = fileName.substring(0,fileName.indexOf(".")).concat("_chk.txt");
+    	System.out.printf(Util.FILE_SUCCESS_NOTIFICATION, fileName, outputFileName);
+    	FileOutputStream os = new FileOutputStream(outputFileName);
+    	PrintWriter outWriter = new PrintWriter(os);
+    	
+    	s.close();
+    	
+    }
+  
+    public static void main(String[] args) throws IOException {
+    	SpellChecker.start(); 
+    }
+  }
