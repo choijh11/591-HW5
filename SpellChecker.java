98c59b76f71df1e6142d4dcc2005e69a0b90d3a4
alecrgraham <89941163+alecrgraham@users.noreply.github.com>
2021-11-10 23:22:41 -0500

    Add files via upload

--- SpellChecker.java
+++ SpellChecker.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 public class SpellChecker {
 
@@ -7,21 +10,41 @@ public class SpellChecker {
 		// or you can leave it blank. You must keep the signature, however.
 	}
 
-	public static void start() {
+	public void start() {
+
 		Scanner scnr = new Scanner(System.in);
+
+		// prompt user for a dictionary to use for spell check
 		System.out.printf(Util.DICTIONARY_PROMPT);
 		String dictionaryChoice = scnr.nextLine();
-		while (!dictionaryChoice.equals("engDictionary.txt")) {
+
+		try {
+			FileInputStream dictionaryStream = new FileInputStream(dictionaryChoice);
+		} catch (FileNotFoundException e) {
 			System.out.printf(Util.FILE_OPENING_ERROR);
 			System.out.printf(Util.DICTIONARY_PROMPT);
-			dictionaryChoice = scnr.nextLine(); 
+			dictionaryChoice = scnr.nextLine();
 		}
+
 		System.out.printf(Util.DICTIONARY_SUCCESS_NOTIFICATION, dictionaryChoice);
+
+		// prompt user for a file to spell check
+		System.out.printf(Util.FILENAME_PROMPT);
+		String fileChoice = scnr.nextLine();
+		
+		try {
+			FileOutputStream spellChecked = new FileOutputStream(fileChoice);
+		} catch (FileNotFoundException e) {
+			System.out.printf(Util.FILE_OPENING_ERROR);
+			System.out.printf(Util.FILENAME_PROMPT); 
+			fileChoice = scnr.nextLine(); 
+		}
+		
+		System.out.printf(Util.FILE_SUCCESS_NOTIFICATION, fileChoice); 
 	}
 
-	// You can of course write other methods as well.
-
 	public static void main(String[] args) {
-		SpellChecker.start();
+		SpellChecker sc = new SpellChecker(); 
+		sc.start();
 	}
 }
